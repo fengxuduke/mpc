47f8f81539a09ab7680a46afcedf3e8b9171e69a
fengxuduke <39333585+fengxuduke@users.noreply.github.com>
2022-06-25 10:05:06 +0800

    occupy the whole cpu core

--- src/writer.cpp
+++ src/writer.cpp
@@ -8,9 +8,22 @@ using std::endl;
 int main() {
     cpu_set_affinity(2);
     auto writer=Writer::create("/tmp/trading/testjournal","testj5");
-    for(int i=0;i<=100;++i) {
-        usleep(10000000);
-        writer->WriteFrame(static_cast<void *>(&i), sizeof(int));
+    int i=-1;
+    writer->WriteFrame(static_cast<void *>(&i), sizeof(int)); 
+    long long duration = getNanoTime()-(writer->frame).getNano();
+    std::cout<<"time duration:"<<duration <<std::endl;   
+    for(int i=0;i<=100;) {
+        
+        if (duration>10000000000){
+           writer->WriteFrame(static_cast<void *>(&i), sizeof(int));
+           duration=getNanoTime()-(writer->frame).getNano();
+           std::cout<<"time duration:"<<duration <<std::endl;
+           ++i;   
+	}
+        else
+           duration=getNanoTime()-(writer->frame).getNano();
+        
+        
     }
 
     return 0;
