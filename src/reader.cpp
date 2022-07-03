cb74ab604db8d4e1105f528654b28043e9004906
fengxuduke <39333585+fengxuduke@users.noreply.github.com>
2022-07-03 08:38:53 +0800

    Add files via upload

--- src/reader.cpp
+++ src/reader.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "communicator/Reader.h"
 #include "utils/stat.h"
+#include "ctp/ThostFtdcUserApiStruct.h"
 //#include "utils.h"
 using std::cout;
 using std::endl;
@@ -9,7 +10,7 @@ int main() {
     cpu_set_affinity(1);
     auto reader=Reader::create("testReader");
     std::cout<<"before addJournal"<<std::endl;
-    reader->addJournal("/home/feng/Documents/trading/testjournal","testj5");
+    reader->addJournal("/tmp/trading/testjournal","testj5");
     std::cout<<"after addJournal"<<std::endl;
     while(true){
         // auto frameptr= static_cast<Frame *> (reader->readFrame());
@@ -19,7 +20,8 @@ int main() {
         else{
         auto frame = Frame(frameptr);
         long duration=getNanoTime()-frame.getNano();
-        std::cout<<"time duration:"<<duration <<std::endl;
+        auto data = static_cast<CThostFtdcDepthMarketDataField*> (frame.getData() );
+        std::cout<<"time duration:"<<duration<<" "<<data->LastPrice<<std::endl;
         }
     }
 
--- src/writer.cpp
+++ src/writer.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 #include "communicator/Writer.h"
 #include "utils/stat.h"
+#include "ctp/ThostFtdcUserApiStruct.h"
 //#include "utils.h"
 using std::cout;
 using std::endl;
 
 int main() {
     cpu_set_affinity(2);
-    auto writer=Writer::create("/home/feng/Documents/trading/testjournal","testj5");
-    for(int i=0;i<=100;++i) {
-        usleep(10000000);
-        writer->WriteFrame(static_cast<void *>(&i), sizeof(int));
+    auto writer=Writer::create("/tmp/trading/testjournal","testj5");
+    int i=-1;
+    CThostFtdcDepthMarketDataField datas[102];
+    size_t length = sizeof(CThostFtdcDepthMarketDataField);
+    std::cout<<"data size="<<length<<std::endl;
+    datas[0].LastPrice=i;
+    writer->WriteFrame(static_cast<void *>(&datas[0]), length); 
+    long long duration = getNanoTime()-(writer->frame).getNano();
+    std::cout<<"time duration:"<<duration <<std::endl;   
+    for(int i=0;i<=100;) {
+        
+        if (duration>125000000){
+           datas[i+1].LastPrice=i;
+    	   writer->WriteFrame(static_cast<void *>(&datas[i+1]), length);
+           duration=getNanoTime()-(writer->frame).getNano();
+           std::cout<<"time duration:"<<duration <<std::endl;
+           ++i;   
+	}
+        else
+           duration=getNanoTime()-(writer->frame).getNano();
+        
+        
     }
 
     return 0;
